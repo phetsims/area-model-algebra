e6e5fd7ed2cb86b677e4fa26eb1221876f013db5
zepumph <michael.kauzmann@colorado.edu>
2019-09-19 13:54:38 -0800

    var -> const in require statements, use arrow function in define require call, https://github.com/phetsims/tasks/issues/1010

--- js/area-model-algebra-main.js
+++ js/area-model-algebra-main.js
@@ -5,19 +5,19 @@
  *
  * @author Jonathan Olson <jonathan.olson@colorado.edu>
  */
-define( function( require ) {
+define( require => {
   'use strict';
 
   // modules
-  var ExploreScreen = require( 'AREA_MODEL_COMMON/screens/ExploreScreen' );
-  var GenericScreen = require( 'AREA_MODEL_COMMON/screens/GenericScreen' );
-  var Sim = require( 'JOIST/Sim' );
-  var SimLauncher = require( 'JOIST/SimLauncher' );
-  var VariablesGameScreen = require( 'AREA_MODEL_COMMON/screens/VariablesGameScreen' );
-  var VariablesScreen = require( 'AREA_MODEL_COMMON/screens/VariablesScreen' );
+  const ExploreScreen = require( 'AREA_MODEL_COMMON/screens/ExploreScreen' );
+  const GenericScreen = require( 'AREA_MODEL_COMMON/screens/GenericScreen' );
+  const Sim = require( 'JOIST/Sim' );
+  const SimLauncher = require( 'JOIST/SimLauncher' );
+  const VariablesGameScreen = require( 'AREA_MODEL_COMMON/screens/VariablesGameScreen' );
+  const VariablesScreen = require( 'AREA_MODEL_COMMON/screens/VariablesScreen' );
 
   // strings
-  var areaModelAlgebraTitleString = require( 'string!AREA_MODEL_ALGEBRA/area-model-algebra.title' );
+  const areaModelAlgebraTitleString = require( 'string!AREA_MODEL_ALGEBRA/area-model-algebra.title' );
 
   var simOptions = {
     credits: {
