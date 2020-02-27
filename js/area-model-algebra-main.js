3574de78a154d6b1d3f08e9e307d8c91c6ccf64c
samreid <reids@colorado.edu>
2020-02-27 11:38:06 -0700

    Add repo to migration list, see https://github.com/phetsims/chipper/issues/875

--- js/area-model-algebra-main.js
+++ js/area-model-algebra-main.js
@@ -5,37 +5,33 @@
  *
  * @author Jonathan Olson <jonathan.olson@colorado.edu>
  */
-define( require => {
-  'use strict';
 
-  // modules
-  const ExploreScreen = require( 'AREA_MODEL_COMMON/screens/ExploreScreen' );
-  const GenericScreen = require( 'AREA_MODEL_COMMON/screens/GenericScreen' );
-  const Sim = require( 'JOIST/Sim' );
-  const SimLauncher = require( 'JOIST/SimLauncher' );
-  const VariablesGameScreen = require( 'AREA_MODEL_COMMON/screens/VariablesGameScreen' );
-  const VariablesScreen = require( 'AREA_MODEL_COMMON/screens/VariablesScreen' );
+import ExploreScreen from '../../area-model-common/js/screens/ExploreScreen.js';
+import GenericScreen from '../../area-model-common/js/screens/GenericScreen.js';
+import VariablesGameScreen from '../../area-model-common/js/screens/VariablesGameScreen.js';
+import VariablesScreen from '../../area-model-common/js/screens/VariablesScreen.js';
+import Sim from '../../joist/js/Sim.js';
+import SimLauncher from '../../joist/js/SimLauncher.js';
+import areaModelAlgebraStrings from './area-model-algebra-strings.js';
 
-  // strings
-  const areaModelAlgebraTitleString = require( 'string!AREA_MODEL_ALGEBRA/area-model-algebra.title' );
+const areaModelAlgebraTitleString = areaModelAlgebraStrings[ 'area-model-algebra' ].title;
 
-  const simOptions = {
-    credits: {
-      leadDesign: 'Amy Hanson, Amanda McGarry',
-      softwareDevelopment: 'Jonathan Olson',
-      team: 'Karina Hensberry, Susan Miller, Ariel Paul, Kathy Perkins, Oliver Nix',
-      qualityAssurance: 'Steele Dalton, Bryce Griebenow, Ethan Johnson, Liam Mulhall, Ben Roberts, Jacob Romero, Ethan Ward, Clara Wilson, Kathryn Woessner',
-      graphicArts: 'Mariah Hermsmeyer, Amanda McGarry, Diana L\u00f3pez Tavares'
-    }
-  };
+const simOptions = {
+  credits: {
+    leadDesign: 'Amy Hanson, Amanda McGarry',
+    softwareDevelopment: 'Jonathan Olson',
+    team: 'Karina Hensberry, Susan Miller, Ariel Paul, Kathy Perkins, Oliver Nix',
+    qualityAssurance: 'Steele Dalton, Bryce Griebenow, Ethan Johnson, Liam Mulhall, Ben Roberts, Jacob Romero, Ethan Ward, Clara Wilson, Kathryn Woessner',
+    graphicArts: 'Mariah Hermsmeyer, Amanda McGarry, Diana L\u00f3pez Tavares'
+  }
+};
 
-  SimLauncher.launch( function() {
-    const sim = new Sim( areaModelAlgebraTitleString, [
-      new ExploreScreen(),
-      new GenericScreen(),
-      new VariablesScreen(),
-      new VariablesGameScreen()
-    ], simOptions );
-    sim.start();
-  } );
+SimLauncher.launch( function() {
+  const sim = new Sim( areaModelAlgebraTitleString, [
+    new ExploreScreen(),
+    new GenericScreen(),
+    new VariablesScreen(),
+    new VariablesGameScreen()
+  ], simOptions );
+  sim.start();
 } );
---
+++ js/area-model-algebra-strings.js
@@ -0,0 +1,14 @@
+// Copyright 2020, University of Colorado Boulder
+
+/**
+ * Auto-generated from modulify, DO NOT manually modify.
+ */
+
+import getStringModule from '../../chipper/js/getStringModule.js';
+import areaModelAlgebra from './areaModelAlgebra.js';
+
+const areaModelAlgebraStrings = getStringModule( 'AREA_MODEL_ALGEBRA' );
+
+areaModelAlgebra.register( 'areaModelAlgebraStrings', areaModelAlgebraStrings );
+
+export default areaModelAlgebraStrings;
