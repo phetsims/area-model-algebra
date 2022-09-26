ca9193b0fe2862a4fbb2c978def7ce4d3270ccd1
Sam Reid <reids@colorado.edu>
2022-09-26 12:13:30 -0600

    Change TReadOnlyProperty => LinkableProperty, see https://github.com/phetsims/axon/issues/414

--- js/AreaModelAlgebraStrings.ts
+++ js/AreaModelAlgebraStrings.ts
@@ -5,13 +5,13 @@
  */
 /* eslint-disable */
 import getStringModule from '../../chipper/js/getStringModule.js';
-import TReadOnlyProperty from '../../axon/js/TReadOnlyProperty.js';
+import LinkableProperty from '../../axon/js/LinkableProperty.js';
 import areaModelAlgebra from './areaModelAlgebra.js';
 
 type StringsType = {
   'area-model-algebra': {
     'title': string;
-    'titleStringProperty': TReadOnlyProperty<string>;
+    'titleStringProperty': LinkableProperty<string>;
   }
 };
 
