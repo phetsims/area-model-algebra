dcd367a7b867ddb8910350b50204124e68b3218f
phet-dev <phetadmin@gmail.com>
2024-02-04 03:13:49 -0700

    update copyright dates from daily grunt work

--- js/game/GameScreen.js
+++ js/game/GameScreen.js
@@ -1,4 +1,4 @@
-// Copyright 2023, University of Colorado Boulder
+// Copyright 2023-2024, University of Colorado Boulder
 
 /**
  * Extends the "Game" screen in Area Model Algebra, serves to pass in the JugglerImages.
