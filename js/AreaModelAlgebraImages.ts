4284ccd03bd7333de8c212b8a0139c2c2b35cadc
Luisav1 <luisa.vargassuarez@ucalgary.ca>
2024-03-26 12:01:01 -0600

    Create images.json and Images.ts files. See https://github.com/phetsims/area-model-multiplication/issues/16.

---
+++ js/AreaModelAlgebraImages.ts
@@ -0,0 +1,105 @@
+// Copyright 2024, University of Colorado Boulder
+
+/**
+ * Auto-generated from modulify, DO NOT manually modify.
+ */
+/* eslint-disable */
+import LocalizedImageProperty from '../../joist/js/i18n/LocalizedImageProperty.js';
+import areaModelAlgebra from './areaModelAlgebra.js';
+import africaGameHomeScreenIcon_svg from '../images/africa/africaGameHomeScreenIcon_svg.js';
+import africaLevel2Icon_svg from '../images/africa/africaLevel2Icon_svg.js';
+import africaLevel3Icon_svg from '../images/africa/africaLevel3Icon_svg.js';
+import africaLevel4Icon_svg from '../images/africa/africaLevel4Icon_svg.js';
+import africaLevel5Icon_svg from '../images/africa/africaLevel5Icon_svg.js';
+import africaModestGameHomeScreenIcon_svg from '../images/africaModest/africaModestGameHomeScreenIcon_svg.js';
+import africaModestLevel2Icon_svg from '../images/africaModest/africaModestLevel2Icon_svg.js';
+import africaModestLevel4Icon_svg from '../images/africaModest/africaModestLevel4Icon_svg.js';
+import asiaGameHomeScreenIcon_svg from '../images/asia/asiaGameHomeScreenIcon_svg.js';
+import asiaLevel1Icon_svg from '../images/asia/asiaLevel1Icon_svg.js';
+import asiaLevel3Icon_svg from '../images/asia/asiaLevel3Icon_svg.js';
+import asiaLevel4Icon_svg from '../images/asia/asiaLevel4Icon_svg.js';
+import asiaLevel5Icon_svg from '../images/asia/asiaLevel5Icon_svg.js';
+import asiaLevel6Icon_svg from '../images/asia/asiaLevel6Icon_svg.js';
+import latinAmericaGameHomeScreenIcon_svg from '../images/latinAmerica/latinAmericaGameHomeScreenIcon_svg.js';
+import latinAmericaLevel1Icon_svg from '../images/latinAmerica/latinAmericaLevel1Icon_svg.js';
+import latinAmericaLevel2Icon_svg from '../images/latinAmerica/latinAmericaLevel2Icon_svg.js';
+import latinAmericaLevel3Icon_svg from '../images/latinAmerica/latinAmericaLevel3Icon_svg.js';
+import latinAmericaLevel4Icon_svg from '../images/latinAmerica/latinAmericaLevel4Icon_svg.js';
+import latinAmericaLevel5Icon_svg from '../images/latinAmerica/latinAmericaLevel5Icon_svg.js';
+import latinAmericaLevel6Icon_svg from '../images/latinAmerica/latinAmericaLevel6Icon_svg.js';
+import oceaniaGameHomeScreenIcon_svg from '../images/oceania/oceaniaGameHomeScreenIcon_svg.js';
+import oceaniaLevel1Icon_svg from '../images/oceania/oceaniaLevel1Icon_svg.js';
+import oceaniaLevel3Icon_svg from '../images/oceania/oceaniaLevel3Icon_svg.js';
+import oceaniaLevel4Icon_svg from '../images/oceania/oceaniaLevel4Icon_svg.js';
+import oceaniaLevel5Icon_svg from '../images/oceania/oceaniaLevel5Icon_svg.js';
+import oceaniaLevel6Icon_svg from '../images/oceania/oceaniaLevel6Icon_svg.js';
+import usaGameHomeScreenIcon_svg from '../images/usa/usaGameHomeScreenIcon_svg.js';
+import usaLevel1Icon_svg from '../images/usa/usaLevel1Icon_svg.js';
+import usaLevel2Icon_svg from '../images/usa/usaLevel2Icon_svg.js';
+import usaLevel3Icon_svg from '../images/usa/usaLevel3Icon_svg.js';
+import usaLevel4Icon_svg from '../images/usa/usaLevel4Icon_svg.js';
+import usaLevel5Icon_svg from '../images/usa/usaLevel5Icon_svg.js';
+import usaLevel6Icon_svg from '../images/usa/usaLevel6Icon_svg.js';
+
+const AreaModelAlgebraImages = {
+  gameHomeScreenIconImageProperty: new LocalizedImageProperty( 'gameHomeScreenIcon', {
+    usa: usaGameHomeScreenIcon_svg,
+    africa: africaGameHomeScreenIcon_svg,
+    africaModest: africaModestGameHomeScreenIcon_svg,
+    asia: asiaGameHomeScreenIcon_svg,
+    latinAmerica: latinAmericaGameHomeScreenIcon_svg,
+    oceania: oceaniaGameHomeScreenIcon_svg
+  } ),
+  level1ImageProperty: new LocalizedImageProperty( 'level1', {
+    usa: usaLevel1Icon_svg,
+    africa: usaLevel1Icon_svg,
+    africaModest: usaLevel1Icon_svg,
+    asia: asiaLevel1Icon_svg,
+    latinAmerica: latinAmericaLevel1Icon_svg,
+    oceania: oceaniaLevel1Icon_svg
+  } ),
+  level2ImageProperty: new LocalizedImageProperty( 'level2', {
+    usa: usaLevel2Icon_svg,
+    africa: africaLevel2Icon_svg,
+    africaModest: africaModestLevel2Icon_svg,
+    asia: usaLevel2Icon_svg,
+    latinAmerica: latinAmericaLevel2Icon_svg,
+    oceania: usaLevel2Icon_svg
+  } ),
+  level3ImageProperty: new LocalizedImageProperty( 'level3', {
+    usa: usaLevel3Icon_svg,
+    africa: africaLevel3Icon_svg,
+    africaModest: africaLevel3Icon_svg,
+    asia: asiaLevel3Icon_svg,
+    latinAmerica: latinAmericaLevel3Icon_svg,
+    oceania: oceaniaLevel3Icon_svg
+  } ),
+  level4ImageProperty: new LocalizedImageProperty( 'level4', {
+    usa: usaLevel4Icon_svg,
+    africa: africaLevel4Icon_svg,
+    africaModest: africaModestLevel4Icon_svg,
+    asia: asiaLevel4Icon_svg,
+    latinAmerica: latinAmericaLevel4Icon_svg,
+    oceania: oceaniaLevel4Icon_svg
+  } ),
+  level5ImageProperty: new LocalizedImageProperty( 'level5', {
+    usa: usaLevel5Icon_svg,
+    africa: africaLevel5Icon_svg,
+    africaModest: africaLevel5Icon_svg,
+    asia: asiaLevel5Icon_svg,
+    latinAmerica: latinAmericaLevel5Icon_svg,
+    oceania: oceaniaLevel5Icon_svg
+  } ),
+  level6ImageProperty: new LocalizedImageProperty( 'level6', {
+    usa: usaLevel6Icon_svg,
+    africa: usaLevel6Icon_svg,
+    africaModest: usaLevel6Icon_svg,
+    asia: asiaLevel6Icon_svg,
+    latinAmerica: latinAmericaLevel6Icon_svg,
+    oceania: oceaniaLevel6Icon_svg
+  } )
+};
+
+areaModelAlgebra.register( 'AreaModelAlgebraImages', AreaModelAlgebraImages );
+
+export default AreaModelAlgebraImages;
