6e160551e6f7211344f8e0d770f6db846bde1377
Sam Reid <reids@colorado.edu>
2024-09-25 19:32:26 -0600

    Upgrade to ESLint 9 and flat config, see https://github.com/phetsims/chipper/issues/1474

---
+++ eslint.config.mjs
@@ -0,0 +1,14 @@
+// Copyright 2024, University of Colorado Boulder
+
+/**
+ * ESlint configuration for area-model-algebra.
+ *
+ * @author Sam Reid (PhET Interactive Simulations)
+ * @author Michael Kauzmann (PhET Interactive Simulations)
+ */
+
+import simEslintConfig from '../chipper/eslint/sim.eslint.config.mjs';
+
+export default [
+  ...simEslintConfig
+];
