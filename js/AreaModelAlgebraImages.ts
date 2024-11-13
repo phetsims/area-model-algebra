1792045e508ea88f0852ecabdc2730ea4f89c6ff
phet-dev <phetadmin@gmail.com>
2024-11-13 04:27:07 -0700

    grunt update  from daily grunt work

--- js/AreaModelAlgebraImages.ts
+++ js/AreaModelAlgebraImages.ts
@@ -1,10 +1,10 @@
 // Copyright 2024, University of Colorado Boulder
-
+/* eslint-disable */
+/* @formatter:off */
 /**
  * Auto-generated from modulify, DO NOT manually modify.
  */
-/* eslint-disable */
-/* @formatter:off */
+ 
 import LocalizedImageProperty from '../../joist/js/i18n/LocalizedImageProperty.js';
 import areaModelAlgebra from './areaModelAlgebra.js';
 import africaLevel2Icon_svg from '../images/africa/africaLevel2Icon_svg.js';
