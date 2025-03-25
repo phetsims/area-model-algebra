8bb3145deb80fea80a37cb9d8aaaa250b9d115ec
Michael Kauzmann <michael.kauzmann@colorado.edu>
2025-03-25 16:32:52 -0600

    runnable *QueryParameters.ts files to use QueryStringMachineModule, https://github.com/phetsims/query-string-machine/issues/54

--- js/AreaModelAlgebraQueryParameters.ts
+++ js/AreaModelAlgebraQueryParameters.ts
@@ -6,6 +6,7 @@
  * @author Chris Malley (PixelZoom, Inc.)
  */
 
+import { QueryStringMachine } from '../../query-string-machine/js/QueryStringMachineModule.js';
 import getGameLevelsSchema from '../../vegas/js/getGameLevelsSchema.js';
 import areaModelAlgebra from './areaModelAlgebra.js';
 
